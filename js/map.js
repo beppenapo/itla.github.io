f657f6f0acf9980273d20d242069e53b0bc0b6c9
Giuseppe Naponiello <beppenapo@arc-team.com>
2019-08-22 21:14:00 +0200

    menu

--- js/map.js
+++ js/map.js
@@ -1,3 +1,5 @@
+//map.fitBounds(e.layer.getBounds());
+
 let map = L.map('map').setView([0,0],2);
 let world = './inc/continents.json';
 let worldStyle = { stroke: false, fill: true, fillColor: '#fff', fillOpacity: 1}
@@ -11,47 +13,62 @@ let italyHighlight = { stroke: true, color:'rgba(210,143,12,1)', weight:1, fill:
 let selected, worldLayer, continentsLayer;
 let thunderF = L.tileLayer('https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=f1151206891e4ca7b1f6eda1e0852b2e');
 let zoom = 0;
+
 $.getJSON(world,function(data){
   worldLayer = L.geoJson(data, { clickable: true, style: worldStyle})
-  .on('click', function (e) {
-    if (selected) { e.target.resetStyle(selected) }
-    selected = e.layer
-    selected.bringToFront()
-    selected.setStyle(highlight)
-    openPanel(e)
-    L.DomEvent.stopPropagation(e);
-  })
-  .addTo(map);
+    .on('click', function (e) {
+      if (selected) { e.target.resetStyle(selected) }
+      selected = e.layer
+      selected.bringToFront()
+      selected.setStyle(highlight)
+      openPanel(e)
+      L.DomEvent.stopPropagation(e);
+    })
+    .addTo(map);
   map.fitBounds(worldLayer.getBounds());
 })
 $.getJSON(continents,function(data){
   continentsLayer = L.geoJson(data, { clickable: true, style: contStyle})
-  .on('click', function (e) {
-    if (selected) { e.target.resetStyle(selected) }
-    selected = e.layer
-    selected.bringToFront()
-    selected.setStyle(contHighlight)
-    openPanel(e)
-    L.DomEvent.stopPropagation(e);
-  });
+    .on('click', function (e) {
+      if (selected) { e.target.resetStyle(selected) }
+      selected = e.layer
+      selected.bringToFront()
+      selected.setStyle(contHighlight)
+      openPanel(e)
+      L.DomEvent.stopPropagation(e);
+    });
 })
 $.getJSON(italy,function(data){
   italyLayer = L.geoJson(data, { clickable: true, style: italyStyle})
-  .on('click', function (e) {
-    if (selected) { e.target.resetStyle(selected) }
-    selected = e.layer
-    selected.bringToFront()
-    selected.setStyle(italyHighlight)
-    openPanel(e)
-    L.DomEvent.stopPropagation(e);
-  });
+    .on('click', function (e) {
+      if (selected) { e.target.resetStyle(selected) }
+      selected = e.layer
+      selected.bringToFront()
+      selected.setStyle(italyHighlight)
+      openPanel(e)
+      L.DomEvent.stopPropagation(e);
+    });
 })
 
-map.on('click',closePanel)
+var resetMap = L.Control.extend({
+  options: { position: 'topleft'},
+  onAdd: function (map) {
+    var container = L.DomUtil.create('div', 'extentControl leaflet-bar leaflet-control leaflet-touch');
+    btn=$("<a/>",{href:'#'}).appendTo(container);
+    $("<i/>",{class:'fas fa-home'}).appendTo(btn)
+    btn.on('click', function (e) {
+      e.preventDefault()
+      map.fitBounds(worldLayer.getBounds());
+    });
+    return container;
+  }
+})
+map.addControl(new resetMap());
+L.control.scale({imperial:false}).addTo(map);
 
+map.on('click',closePanel)
 map.on('zoomend',function(){
   zoom = map.getZoom();
-  console.log(zoom);
   switch (true) {
     case (zoom < 5):
       map.removeLayer(italyLayer)
